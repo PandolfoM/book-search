05b39daaa4317976df0dbbdbb39100363777495a
Matthew Pandolfo <matt@pandolfo.com>
2021-12-19 20:26:43 -0500

    swaed

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,9 +1,9 @@
-import { useMutation } from "@apollo/client";
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
 import { Form, Button, Alert } from "react-bootstrap";
 
-import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 const SignupForm = () => {
   // set initial form state
