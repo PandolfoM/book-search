50df44239bba8916e4816ed2ca6a22739c9ee9e2
Matthew Pandolfo <matt@pandolfo.com>
2021-12-19 21:02:15 -0500

    backend not working

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
@@ -31,11 +34,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
