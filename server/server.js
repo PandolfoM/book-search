5c846c145161abeee2575d48e545db56ba4d75b4
Matthew Pandolfo <matt@pandolfo.com>
2021-12-19 20:07:05 -0500

    production env

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 const routes = require("./routes");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,17 +28,16 @@ startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.use(routes);
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
-app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () =>
