7ae4c89d2583d047c4dc0394d0b718a833350efd
Matthew Pandolfo <matt@pandolfo.com>
2021-12-19 22:02:15 -0500

    remove dotenv

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
